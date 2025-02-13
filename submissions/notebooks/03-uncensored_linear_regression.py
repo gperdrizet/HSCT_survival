1e11a8e545282e7f2f2cd981b960285b942e494e
George Perdrizet <george@perdrizet.org>
2025-02-13 11:35:58 -0500

    Converted back to script from Kaggle generated notebook.

--- submissions/notebooks/03-uncensored_linear_regression.py
+++ submissions/notebooks/03-uncensored_linear_regression.py
@@ -1,1 +1,104 @@
-{"metadata":{"kernelspec":{"language":"python","display_name":"Python 3","name":"python3"},"language_info":{"name":"python","version":"3.10.12","mimetype":"text/x-python","codemirror_mode":{"name":"ipython","version":3},"pygments_lexer":"ipython3","nbconvert_exporter":"python","file_extension":".py"},"kaggle":{"accelerator":"none","dataSources":[{"sourceId":70942,"databundleVersionId":10381525,"sourceType":"competition"},{"sourceId":10700324,"sourceType":"datasetVersion","datasetId":6631008}],"dockerImageVersionId":30886,"isInternetEnabled":false,"language":"python","sourceType":"notebook","isGpuEnabled":false}},"nbformat_minor":4,"nbformat":4,"cells":[{"cell_type":"code","source":"import os\nimport pickle\nimport pandas as pd\n\n# Figure out if we are in a Kaggle notebook or not based on the current path and set file paths accordingly\nif os.getcwd() == '/kaggle/working':\n    print('Running on Kaggle')\n    data_dictionary_file='/kaggle/input/equity-post-HCT-survival-predictions/data_dictionary.csv'\n    model_file='/kaggle/input/hsct-survival-uncensored-regression/03.4-uncensored_linear_regression.pkl'\n    knn_imputer_file='/kaggle/input/hsct-survival-uncensored-regression/02-KNN_imputer.pkl'\n    one_hot_encoder_file='/kaggle/input/hsct-survival-uncensored-regression/02-one_hot_encoder.pkl'\n    testing_data_file='/kaggle/input/equity-post-HCT-survival-predictions/test.csv'\n    submission_file='submission.csv'\n\nelse:\n    data_dictionary_file='../../data/raw/data_dictionary.csv'\n    model_file='../../models/03.4-uncensored_linear_regression.pkl'\n    knn_imputer_file='../../models/02-KNN_imputer.pkl'\n    one_hot_encoder_file='../../models/02-one_hot_encoder.pkl'\n    testing_data_file='../../data/raw/test.csv'\n    submission_file='../predictions/polynomial_regression_submission.csv'\n\n\nif __name__=='__main__':\n\n    ###################################\n    # LOAD ASSETS #####################\n    ###################################\n\n    # Load the model\n    with open(model_file, 'rb') as input_file:\n        model=pickle.load(input_file)\n\n    # Load the one-hot encoder\n    with open(one_hot_encoder_file, 'rb') as input_file:\n        encoder=pickle.load(input_file)\n\n    # Load the KNN imputer\n    with open(knn_imputer_file, 'rb') as input_file:\n        imputer=pickle.load(input_file)\n\n    # Load testing data and column definitions\n    testing_data=pd.read_csv(testing_data_file)\n    data_dictionary=pd.read_csv(data_dictionary_file)\n\n    ###################################\n    # PROCESS TESTING DATA ############\n    ###################################\n\n    # Save the ID and drop\n    testing_ids=testing_data['ID']\n    testing_data.drop('ID', axis=1, inplace=True)\n    print(f'Testing features: {testing_data.shape}')\n\n    # Get lists of categorical and numerical column names\n    categorical_feature_names=data_dictionary['variable'][data_dictionary['type'] == 'Categorical']\n    numerical_feature_names=data_dictionary['variable'][data_dictionary['type'] == 'Numerical']\n\n    # Remove the feature column from the column names lists\n    categorical_feature_names=categorical_feature_names[categorical_feature_names != 'efs']\n    numerical_feature_names=numerical_feature_names[numerical_feature_names != 'efs_time']\n\n    # Split the testing dataframe\n    testing_categorical_df=testing_data[categorical_feature_names].copy()\n    testing_numerical_df=testing_data[numerical_feature_names].copy()\n\n    print(f'Testing numerical features: {testing_numerical_df.shape}')\n    print(f'Testing categorical features: {testing_categorical_df.shape}')\n\n    # Replace NAN with 'Missing' string\n    testing_categorical_df.fillna('Missing', inplace=True)\n    print(f'Testing categorical features: {testing_categorical_df.shape}')\n\n    # Fill missing data\n    testing_numerical_data=imputer.transform(testing_numerical_df)\n\n    # Re-build dataframe\n    testing_numerical_df=pd.DataFrame(testing_numerical_data, columns=testing_numerical_df.columns)\n    print(f'Testing numerical features: {testing_numerical_df.shape}')\n\n    # Encode the features\n    testing_categorical_data=encoder.transform(testing_categorical_df)\n\n    # Rebuild the dataframe\n    feature_names=encoder.get_feature_names_out()\n    testing_categorical_df=pd.DataFrame(testing_categorical_data, columns=feature_names)\n    print(f'Testing categorical features: {testing_categorical_df.shape}')\n\n    # Recombine numerical and categorical features\n    testing_features_df=pd.concat(\n        [\n            testing_numerical_df.reset_index(drop=True), \n            testing_categorical_df.reset_index(drop=True)\n        ],\n        axis=1\n    )\n\n    print(f'Testing features: {testing_features_df.shape}')\n\n    predicted_efs_time=model.predict(testing_features_df)\n    predictions_df=pd.DataFrame.from_dict({'ID': testing_ids, 'prediction': predicted_efs_time.flatten()})\n    predictions_df.describe()\n    predictions_df.to_csv(submission_file, index=False)","metadata":{"_uuid":"48e85aa4-d7bd-4c7c-a054-08e6f41b03d0","_cell_guid":"1cf55fa7-116e-4410-a65d-585d7ece3170","trusted":true,"collapsed":false,"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2025-02-09T06:07:35.275561Z","iopub.execute_input":"2025-02-09T06:07:35.275886Z","iopub.status.idle":"2025-02-09T06:07:36.561219Z","shell.execute_reply.started":"2025-02-09T06:07:35.275860Z","shell.execute_reply":"2025-02-09T06:07:36.560230Z"}},"outputs":[{"name":"stdout","text":"Running on Kaggle\nTesting features: (3, 57)\nTesting numerical features: (3, 22)\nTesting categorical features: (3, 35)\nTesting categorical features: (3, 35)\nTesting numerical features: (3, 22)\nTesting categorical features: (3, 156)\nTesting features: (3, 178)\n","output_type":"stream"}],"execution_count":1}]}
+import os
+import pickle
+import pandas as pd
+
+# Figure out if we are in a Kaggle notebook or not based on the current path and set file paths accordingly
+if os.getcwd() == '/kaggle/working':
+    print('Running on Kaggle')
+    data_dictionary_file='/kaggle/input/equity-post-HCT-survival-predictions/data_dictionary.csv'
+    model_file='/kaggle/input/hsct-survival-uncensored-regression/03.4-uncensored_linear_regression.pkl'
+    knn_imputer_file='/kaggle/input/hsct-survival-uncensored-regression/02-KNN_imputer.pkl'
+    one_hot_encoder_file='/kaggle/input/hsct-survival-uncensored-regression/02-one_hot_encoder.pkl'
+    testing_data_file='/kaggle/input/equity-post-HCT-survival-predictions/test.csv'
+    submission_file='submission.csv'
+
+else:
+    data_dictionary_file='../../data/raw/data_dictionary.csv'
+    model_file='../../models/03.4-uncensored_linear_regression.pkl'
+    knn_imputer_file='../../models/02-KNN_imputer.pkl'
+    one_hot_encoder_file='../../models/02-one_hot_encoder.pkl'
+    testing_data_file='../../data/raw/test.csv'
+    submission_file='../predictions/polynomial_regression_submission.csv'
+
+
+if __name__=='__main__':
+
+    ###################################
+    # LOAD ASSETS #####################
+    ###################################
+
+    # Load the model
+    with open(model_file, 'rb') as input_file:
+        model=pickle.load(input_file)
+
+    # Load the one-hot encoder
+    with open(one_hot_encoder_file, 'rb') as input_file:
+        encoder=pickle.load(input_file)
+
+    # Load the KNN imputer
+    with open(knn_imputer_file, 'rb') as input_file:
+        imputer=pickle.load(input_file)
+
+    # Load testing data and column definitions
+    testing_data=pd.read_csv(testing_data_file)
+    data_dictionary=pd.read_csv(data_dictionary_file)
+
+    ###################################
+    # PROCESS TESTING DATA ############
+    ###################################
+
+    # Save the ID and drop
+    testing_ids=testing_data['ID']
+    testing_data.drop('ID', axis=1, inplace=True)
+    print(f'Testing features: {testing_data.shape}')
+
+    # Get lists of categorical and numerical column names
+    categorical_feature_names=data_dictionary['variable'][data_dictionary['type'] == 'Categorical']
+    numerical_feature_names=data_dictionary['variable'][data_dictionary['type'] == 'Numerical']
+
+    # Remove the feature column from the column names lists
+    categorical_feature_names=categorical_feature_names[categorical_feature_names != 'efs']
+    numerical_feature_names=numerical_feature_names[numerical_feature_names != 'efs_time']
+
+    # Split the testing dataframe
+    testing_categorical_df=testing_data[categorical_feature_names].copy()
+    testing_numerical_df=testing_data[numerical_feature_names].copy()
+
+    print(f'Testing numerical features: {testing_numerical_df.shape}')
+    print(f'Testing categorical features: {testing_categorical_df.shape}')
+
+    # Replace NAN with 'Missing' string
+    testing_categorical_df.fillna('Missing', inplace=True)
+    print(f'Testing categorical features: {testing_categorical_df.shape}')
+
+    # Fill missing data
+    testing_numerical_data=imputer.transform(testing_numerical_df)
+
+    # Re-build dataframe
+    testing_numerical_df=pd.DataFrame(testing_numerical_data, columns=testing_numerical_df.columns)
+    print(f'Testing numerical features: {testing_numerical_df.shape}')
+
+    # Encode the features
+    testing_categorical_data=encoder.transform(testing_categorical_df)
+
+    # Rebuild the dataframe
+    feature_names=encoder.get_feature_names_out()
+    testing_categorical_df=pd.DataFrame(testing_categorical_data, columns=feature_names)
+    print(f'Testing categorical features: {testing_categorical_df.shape}')
+
+    # Recombine numerical and categorical features
+    testing_features_df=pd.concat(
+        [
+            testing_numerical_df.reset_index(drop=True), 
+            testing_categorical_df.reset_index(drop=True)
+        ],
+        axis=1
+    )
+
+    print(f'Testing features: {testing_features_df.shape}')
+
+    predicted_efs_time=model.predict(testing_features_df)
+    predictions_df=pd.DataFrame.from_dict({'ID': testing_ids, 'prediction': predicted_efs_time.flatten()})
+    predictions_df.describe()
+    predictions_df.to_csv(submission_file, index=False)
+    
