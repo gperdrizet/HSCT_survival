e3ef82790aa0d4ab58fa93bdb33183025a58cd25
George Perdrizet <george@perdrizet.org>
2025-02-23 14:49:27 -0500

    Added paths for experiment results data, plots and processed data.

--- notebooks/configuration.py
+++ notebooks/configuration.py
@@ -3,5 +3,9 @@
 # Project paths
 PROJECT_ROOT_PATH='..'
 NOTEBOOKS_PATH=f'{PROJECT_ROOT_PATH}/notebooks'
-DATA_PATH=f'{PROJECT_ROOT_PATH}/data'
 MODELS_PATH=f'{PROJECT_ROOT_PATH}/models'
+DATA_PATH=f'{PROJECT_ROOT_PATH}/data'
+
+PROCESSED_DATA=f'{DATA_PATH}/processed'
+PLOTS=f'{DATA_PATH}/results/plots'
+RESULTS=f'{DATA_PATH}/results/data'
