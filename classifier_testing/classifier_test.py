9c8ab728adca85ae50cd9ed85504fbab5f61b600
George Perdrizet <george@perdrizet.org>
2025-03-01 13:21:32 -0500

    Added classifiers.

--- classifier_testing/classifier_test.py
+++ classifier_testing/classifier_test.py
@@ -1,7 +1,47 @@
 '''Runs test of Scikit-learn classifiers.'''
 
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
 def run():
     '''Main function to run test of classifiers.'''
+
+    names=[
+        'Nearest Neighbors',
+        'Linear SVM',
+        'RBF SVM',
+        'Gaussian Process',
+        'Decision Tree',
+        'Random Forest',
+        'Neural Net',
+        'AdaBoost',
+        'Naive Bayes',
+        'QDA',
+    ]
+
+    classifiers=[
+        KNeighborsClassifier(3),
+        SVC(kernel='linear', C=0.025, random_state=42),
+        SVC(gamma=2, C=1, random_state=42),
+        GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
+        DecisionTreeClassifier(max_depth=5, random_state=42),
+        RandomForestClassifier(
+            max_depth=5, n_estimators=10, max_features=1, random_state=42
+        ),
+        MLPClassifier(alpha=1, max_iter=1000, random_state=42),
+        AdaBoostClassifier(random_state=42),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis(),
+    ]
 
     return
 
