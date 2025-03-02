416368cebc1c9f66534c1167e2ac9c0a5fb7b770
George Perdrizet <george@perdrizet.org>
2025-03-02 09:56:56 -0500

    Added hyperparameter optimization and ability to resume runs.

--- classifier_testing/classifier_test.py
+++ classifier_testing/classifier_test.py
@@ -4,19 +4,23 @@ import time
 import pickle
 
 import numpy as np
-import xgboost as xgb
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import ShuffleSplit, cross_val_score
+from sklearn.model_selection import RandomizedSearchCV, ShuffleSplit, cross_val_score
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
+# from sklearn.gaussian_process.kernels import RBF
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+
+import classifier_testing.hyperparameters as hyperparams
 
 def run(input_file:str, output_file:str):
     '''Main function to run test of classifiers.'''
@@ -30,55 +34,74 @@ def run(input_file:str, output_file:str):
     with open(input_file, 'rb') as data_file:
         data_dict=pickle.load(data_file)
 
-    names=[
-        'Nearest Neighbors',
-        'Linear SVM',
-        'RBF SVM',
-        'Polynomial SVM',
-        'Gaussian Process',
-        'Decision Tree',
-        'Random Forest',
-        'Neural Net',
-        'AdaBoost',
-        'Naive Bayes',
-        'QDA',
-        'XGBoost'
-    ]
+    classifiers={
+        'Nearest Neighbors':KNeighborsClassifier(),
+        'Radius Neighbors':RadiusNeighborsClassifier(),
+        'Linear SVM':SVC(kernel='linear', max_iter=5000),
+        'RBF SVM':SVC(kernel='rbf', max_iter=5000),
+        'Polynomial SVM':SVC(kernel='poly', max_iter=5000),
+        'Gaussian Process':GaussianProcessClassifier(),
+        'Decision Tree':DecisionTreeClassifier(),
+        'Random Forest':RandomForestClassifier(),
+        'Neural Net':MLPClassifier(max_iter=1000),
+        'AdaBoost':AdaBoostClassifier(),
+        'Naive Bayes':GaussianNB(),
+        'QDA':QuadraticDiscriminantAnalysis(),
+        'SGD classifier': SGDClassifier(),
+        'XGBoost':XGBClassifier(),
+        'CatBoost':CatBoostClassifier()
+    }
 
     print('Classifiers')
-    for name in names:
+    for name in classifiers.keys():
         print(f' {name}')
-
-    classifiers=[
-        KNeighborsClassifier(16),
-        SVC(kernel='linear', max_iter=5000),
-        SVC(kernel='rbf', max_iter=5000),
-        SVC(kernel='poly', max_iter=5000),
-        GaussianProcessClassifier(),
-        DecisionTreeClassifier(),
-        RandomForestClassifier(),
-        MLPClassifier(max_iter=1000),
-        AdaBoostClassifier(),
-        GaussianNB(),
-        QuadraticDiscriminantAnalysis(),
-        xgb.XGBClassifier()
-    ]
 
     print()
 
-    scalar=StandardScaler()
+    feature_scaler=StandardScaler()
+    training_features=feature_scaler.fit_transform(data_dict['Training features'])
+    testing_features=feature_scaler.transform(data_dict['Testing features'])
+
+    training_labels=data_dict['Training labels']['efs']
+
     splitter=ShuffleSplit(n_splits=16, test_size=0.3)
     results={}
 
-    for name, classifier in zip(names, classifiers):
+    for name, classifier in classifiers.items():
 
         start_time=time.time()
-        pipeline=Pipeline([('Scaler', scalar), ('Classifier', classifier)])
+        results[name]={}
+        results[name]['Hyperparameter distributions']=hyperparams.distributions[name]
+
+        with open(output_file, 'rb') as results_file:
+            old_results=pickle.load(results_file)
+
+        if name in old_results.keys():
+            if hyperparams.distributions[name]==old_results[name]['Hyperarameter ditributions']:
+                print(f'{name}: skipped')
+                continue
+
+        optimization=RandomizedSearchCV(
+            classifier,
+            hyperparams.distributions[name],
+            cv=splitter,
+            n_iter=16
+        )
+
+        optimization_result=optimization.fit(
+            training_features,
+            training_labels
+        )
+
+        winning_parameters=optimization_result.best_params_
+        results[name]['Best parameters']=winning_parameters
+
+        classifier.set_params(**winning_parameters)
 
         scores=cross_val_score(
-            pipeline,
-            data_dict['Training features'],
-            data_dict['Training labels']['efs'],
+            classifier,
+            training_features,
+            training_labels,
             cv=splitter,
             scoring='accuracy',
             n_jobs=-1
@@ -87,10 +110,9 @@ def run(input_file:str, output_file:str):
         score_mean=np.mean(scores)
         score_std=np.std(scores)
 
-        classifier.fit(data_dict['Training features'], data_dict['Training labels']['efs'])
-        testing_predictions=classifier.predict(data_dict['Testing features'])
+        classifier.fit(training_features, training_labels)
+        testing_predictions=classifier.predict(testing_features)
 
-        results[name]={}
         results[name]['Cross validation scores']=scores
         results[name]['Testing predictions']=testing_predictions
 
---
+++ classifier_testing/hyperparameters.py
@@ -0,0 +1,74 @@
+'''Hyperparameter distributions for SciKit-learn RandomSearchCV.'''
+
+distributions={
+    'Nearest Neighbors':{
+        'n_neighbors': [2,4,8],
+        'weights': ['uniform','distance'],
+        'leaf_size': [15,30,60],
+        'p': [1,2,3,4]
+    },
+    'Radius Neighbors':{
+        'radius':[0.5,0.75,1.0],
+        'weights': ['uniform','distance'],
+        'leaf_size': [15,30,60],
+        'p': [1,2,3,4]
+    },
+    'Linear SVM':{
+        'kernel': ['linear'],
+        'C': [0.001,0.1,1.0],
+        'class_weight': ['balanced'],
+        'decision_function_shape': ['ovo','ovr']
+    },
+    'RBF SVM':{
+        'kernel': ['rbf'],
+        'gamma': ['scale','auto'],
+        'C': [0.001, 0.1, 1.0],
+        'class_weight': ['balanced'],
+        'decision_function_shape': ['ovo','ovr']
+    },
+    'Polynomial SVM':{
+        'kernel': ['poly'],
+        'gamma': ['scale','auto'],
+        'degree': [2,3,4],
+        'coef0': [0.0,0.001,0.01],
+        'C': [0.001,0.1,1.0],
+        'class_weight': ['balanced'],
+        'decision_function_shape': ['ovo','ovr']
+    },
+    'Gaussian Process':{
+        'n_restarts_optimizer': [0,1,2],
+        'max_iter_predict': [50,100,200]
+    },
+    'Decision Tree':{
+        'criterion': ['gini','entropy','log_loss'],
+        'splitter': ['best','random'],
+        'max_depth': [5,10,15,20],
+        'class_weight': ['balanced'],
+        'max_features': [0.25,0.5,0.75,1]
+    },
+    'Random Forest':{
+        'n_estimators': [50,100,200],
+        'criterion': ['gini','entropy','log_loss'],
+        'class_weight': ['balanced'],
+        'max_features': [0.25,0.5,0.75,1],
+        'ccp_alpha': [0.0,0.01,0.1]
+    },
+    'Neural Net':{
+        'hidden_layer_sizes': [16,32,64,128,256],
+        'alpha': [0.000025,0.00005,0.0001,0.0002],
+        'learning_rate': ['constant', 'invscaling', 'adaptive']
+    },
+    'AdaBoost':{
+        'n_estimators': [25, 50, 100],
+        'learning_rate': [0.25, 0.5, 1]
+    },
+    # 'Naive Bayes':{},
+    'QDA':{'reg_param': [0.0,0.001,0.01]},
+    'SGD classifier':{
+        'alpha': [0.00005,0.0001,0.0002],
+        'l1_ratio': [0.075,0.15,0.30],
+        'learning_rate': ['optimal', 'invscaling', 'adaptive']
+    },
+    # 'XGBoost':,
+    # 'CatBoost':
+}
