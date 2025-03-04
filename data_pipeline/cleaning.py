d1cab575007f6cb9e5358f83e06d1f1bb91e9de1
George Perdrizet <george@perdrizet.org>
2025-03-04 09:08:25 -0500

    Switched to specifying paths via globals in main method and passing as arguments to submodules.

--- data_pipeline/cleaning.py
+++ data_pipeline/cleaning.py
@@ -3,20 +3,20 @@
 import pickle
 import pandas as pd
 
-def run(data_df: pd.DataFrame) -> pd.DataFrame:
+def run(
+        data_df:pd.DataFrame,
+        feature_level_dicts:str,
+        nan_dicts:str
+) -> pd.DataFrame:
     '''Main function to do feature cleaning.'''
 
-    feature_value_translation_dicts_file='../data/processed/01.1-feature_value_translation_dicts.pkl'
-    nan_placeholders_dict_file='../data/processed/01.1-nan_placeholders_list.pkl'
-
-    with open(feature_value_translation_dicts_file, 'rd') as input_file:
+    with open(feature_level_dicts, 'rb') as input_file:
         feature_value_translation_dicts=pickle.load(input_file)
 
     for feature, translation_dict in feature_value_translation_dicts.items():
         data_df[feature]=data_df[feature].replace(translation_dict)
 
-    
-    with open(nan_placeholders_dict_file, 'rb') as input_file:
+    with open(nan_dicts, 'rb') as input_file:
         missing_values=pickle.load(input_file)
 
     data_df.replace(missing_values, inplace=True)
--- data_pipeline/pipeline.py
+++ data_pipeline/pipeline.py
@@ -1,13 +1,26 @@
 '''Data data processing pipeline. Handles data cleaning and encoding
 and feature engineering for inference.'''
-
+import pandas as pd
 from data_pipeline import cleaning
-from data_pipeline import encoding
+# from data_pipeline import encoding
 
-def run(data_df):
+INPUT_DATA='./data/raw/train.csv'
+FEATURE_LEVEL_DICTS='./data/processed/01.1-feature_value_translation_dicts.pkl'
+NAN_DICTS='./data/processed/01.1-nan_placeholders_list.pkl'
+
+def run():
     '''Main function to run data pipeline.'''
 
-    data_df=cleaning.run(data_df)
-    data_df=encoding.run(data_df)
+    data_df=pd.read_csv(INPUT_DATA)
+
+    # Translate feature level values and replace string NAN
+    # placeholders with actual np.nan
+    data_df=cleaning.run(
+        data_df=data_df,
+        feature_level_dicts=FEATURE_LEVEL_DICTS,
+        nan_dicts=NAN_DICTS
+    )
+
+    print(data_df.head().transpose())
 
     return data_df
