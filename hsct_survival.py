04f794f7c526e8c6264dd168336809e242e80109
George Perdrizet <george@perdrizet.org>
2025-03-04 08:52:02 -0500

    Added command line argument for data pipeline task.

--- hsct_survival.py
+++ hsct_survival.py
@@ -1,9 +1,14 @@
 '''Main runner function for HSCT survival project submodules.'''
 
 import argparse
+import warnings
+from sklearn.exceptions import ConvergenceWarning
+from data_pipeline import pipeline
 from estimator_testing import estimator_test
 from estimator_testing import classifiers
 from estimator_testing import regressors
+
+warnings.filterwarnings('ignore', category=ConvergenceWarning)
 
 
 if __name__=='__main__':
@@ -12,15 +17,19 @@ if __name__=='__main__':
     parser.add_argument(
         '--task',
         choices=['prep_data', 'test_classifiers', 'test_regressors'],
-        help='task to run') 
+        help='task to run'
+    )
     args=parser.parse_args()
 
     if args.task is None:
         print('Specify a task with the --task flag.')
 
+    if args.task == 'prep_data':
+        pipeline.run()
+
     if args.task == 'test_classifiers':
         estimator_test.run(
-            input_file='./data/processed/02.1-no-multicollinearity_one_hot_ordinal_nan_imputed_data_df.pkl',
+            input_file=('./data/processed/01.2-binary_target_encoded_power_transformed_data_df.pkl'),
             output_file='./data/results/data/sklearn_classifier_test.pkl',
             models=classifiers.models,
             hyperparameters=classifiers.hyperparameters,
@@ -29,7 +38,7 @@ if __name__=='__main__':
 
     if args.task == 'test_regressors':
         estimator_test.run(
-            input_file='./data/processed/02.1-no-multicollinearity_one_hot_ordinal_nan_imputed_data_df.pkl',
+            input_file='./data/processed/01.2-continuous_target_encoded_power_transform_data_df.pkl',
             output_file='./data/results/data/sklearn_regressor_test.pkl',
             models=regressors.models,
             hyperparameters=regressors.hyperparameters,
